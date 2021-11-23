899c72151b671dd2a22f5ce73893101f00e36636
Sankar4G3 <sankarnarayana2000@gmail.com>
2021-11-23 19:08:14 +0530

    Assignment Submission

---
+++ devopsgraded-main/src/main/java/com/hcl/devops/Devops/DevopsApplication.java
@@ -0,0 +1,20 @@
+package com.hcl.devops.Devops;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Dev-Ops");		
+	}
+
+}
